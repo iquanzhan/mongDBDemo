3369ea8d546c20c587a00bd8c254f36e0098ef68
iquanzhan <iquanzhan@163.com>
2019-01-09 11:02:57 +0800

    条件查询

--- src/main/java/comg/chengxiaoxiao/Demo.java
+++ src/main/java/comg/chengxiaoxiao/Demo.java
@@ -1,9 +1,11 @@
 package comg.chengxiaoxiao;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import org.bson.BasicBSONObject;
 import org.bson.Document;
 
 /**
@@ -16,13 +18,43 @@ public class Demo {
 
 
     public static void main(String[] args) {
+        //基本查询
+        //basicFind();
+        //条件查询
+        searchFind();
 
+
+    }
+
+    /**
+     * 条件查询
+     */
+    private static void searchFind() {
+        MongoClient client = new MongoClient("192.168.217.130");
+        //打开数据库
+        MongoDatabase spitdb = client.getDatabase("spitdb");
+        //获取集合
+        MongoCollection<Document> spit = spitdb.getCollection("spit");
+
+        //比较相等时，
+        //BasicDBObject dbObject = new BasicDBObject("userid", "1011");
+        //查询字段值大于1000的记录
+        BasicDBObject dbObject = new BasicDBObject("userid", new BasicDBObject("$gt", 1000));
+
+
+        FindIterable<Document> documents = spit.find(dbObject);
+
+        for (Document doc : documents) {
+            System.out.println("content=" + doc.getString("content"));
+        }
+
+        client.close();
     }
 
     /**
      * 基本查询
      */
-    public static void basicFind(){
+    public static void basicFind() {
         MongoClient client = new MongoClient("192.168.217.130");
         //打开数据库
         MongoDatabase spitdb = client.getDatabase("spitdb");
